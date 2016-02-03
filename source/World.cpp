45034d3a6742b976a134524d46a801b65de0bc5d
huzaifafaruqui <huzaifafaruqui@outlook.com>
2016-02-03 22:42:05 +0530

    Update World.cpp
    removed comments

--- source/World.cpp
+++ source/World.cpp
@@ -7,7 +7,7 @@
 
 #include <SDL2/SDL_timer.h>
 
-// TODO remove
+
 #include <engine/renderer/GWENRenderer.hpp>
 
 #include <assets/map/MapLoader.hpp>
@@ -77,7 +77,7 @@ void World::create() {
 
 }
 
-// TODO: move elsewhere.
+
 void World::setRendererWindow(Window *win) {
   renderer->setViewport(win);
   window = win;
@@ -89,7 +89,7 @@ void World::destroy() {
 }
 
 void World::loadScene(const std::string &path) {
-  // Delete previous scene
+  
   delete scene;
   currentScenePath = path;
   scene = MapLoader::getScene(path);
@@ -113,9 +113,9 @@ void World::loadScene(const std::string &path) {
   Environment::dispatcher.dispatch(Event::loadScene);
 }
 
-// TODO: deduplicate with ^
+
 void World::loadSceneFromPath(const std::string &path) {
-  // Delete previous scene
+  
   delete scene;
   currentScenePath = path;
   scene = MapLoader::getSceneFromPath(path);
@@ -157,45 +157,9 @@ void World::update(double dtime) {
   phys.update(dtime);
   prtl.update(dtime);
 
-/*
-  for (Entity &e : scene->entities) {
-    // Trigger
-    if (e.hasComponent<Trigger>()) {
-      const Trigger &trigger = e.getComponent<Trigger>();
-      const Transform &trTform = e.getComponent<Transform>();
-      BoxCollider playerCollider(plrTform.position, plrTform.scale);
-      BoxCollider triggerCollider(trTform.position, trTform.scale);
 
-      if (playerCollider.collidesWith(triggerCollider)) {
-        if (trigger.type == "radiation") {
-          player.getComponent<Health>().harm(.1f);
-          System::Log(Verbose) << "Radiation touched.";
-        } else if (trigger.type == "death") {
-          player.getComponent<Health>().kill();
-          System::Log(Verbose) << "Death touched.";
-        } else if (trigger.type == "win") {
-          if (currentLevel + 1 < mapList.size()) {
-            currentLevel++;
-            loadScene(mapList[currentLevel]);
-          } else {
-            scene->screen->enabled = true;
-          }
-          System::Log(Verbose) << "Win touched.";
-        } else if (trigger.type == "map") {
-          System::Log(Verbose) << "Map trigger touched.";
-          throw __FILE__ ": Map trigger type de-implemented, please reimplement";
-          //loadScene(trigger.reference);
-        } else if (trigger.type == "button") {
-          System::Log(Verbose) << "Button touched.";
-          printf("Button touched\n");
-        } else {
-          System::Log(Verbose) << "Other trigger touched. " << trigger.type;
-        }
-      }
-    }
-  }*/
 
-  // Parent camera to player
+ 
   scene->camera.setPerspective();
   int vpWidth, vpHeight;
   renderer->getViewport()->getSize(&vpWidth, &vpHeight);
@@ -203,7 +167,7 @@ void World::update(double dtime) {
   scene->camera.setPosition(plrTform.getPosition() + Vector3f(0, plrTform.getScale().y/2, 0));
   scene->camera.setOrientation(plrComp.getHeadOrientation());
 
-  //Check if the end of the level has been reached
+  
   float distToEnd = (scene->end->getComponent<Transform>().getPosition() -
                      plrTform.getPosition()).length();
   if (distToEnd < 1) {
@@ -226,7 +190,7 @@ Renderer* World::getRenderer() const {
   return renderer;
 }
 
-// TODO: move elsewhere. World is world, not renderer.
+
 void World::render(double dtime) {
   renderer->render(dtime, scene->camera);
   if (isPhysicsDebugEnabled) {
